012883e9dd05f621e803639a5ff4b2eaeb6e2237
Jeremie Bellec <j.bellec@structure-computation.com>
2017-09-05 16:06:52 +0200

    dashboard fix port in config

--- dashboard.js
+++ dashboard.js
@@ -31,15 +31,19 @@ var dashboard = {
     if (!config.user || !config.password) {
       window.location = "login-dashboard.html";
     } else {
-      SpinalUserManager.get_admin_id("http://" + config.host + ":" + config.admin_port + "/", config.user, config.password, function(response) {
+      SpinalUserManager.get_admin_id("http://" + config.host + ":" + config.admin_port +
+		"/", config.user, config.password, function(response) {
         config.user_id = parseInt(response);
-        conn = spinalCore.connect("http://" + config.user_id + ":" + config.password + "@" + config.host + ":" + config.admin_port + "/")
+        conn = spinalCore.connect("http://" + config.user_id + ":" + config.password + 
+		"@" + config.host + ":" + config.admin_port + "/")
         document.getElementById("span-username").innerHTML = "Dashboard - " + config.user;
         launch_spinal_admin();
       }, function(err) {
-        SpinalUserManager.get_user_id("http://" + config.host + ":" + config.user_port + "/", config.user, config.password, function(response) {
+        SpinalUserManager.get_user_id("http://" + config.host + ":" + config.user_port +
+		"/", config.user, config.password, function(response) {
           config.user_id = parseInt(response);
-          conn = spinalCore.connect("http://" + config.user_id + ":" + config.password + "@" + config.host + ":" + config.user_port + "/")
+          conn = spinalCore.connect("http://" + config.user_id + ":" + config.password +
+		"@" + config.host + ":" + config.user_port + "/")
           document.getElementById("span-username").innerHTML = "Dashboard - " + config.user;
           launch_spinal_dasboard();
         }, function(err) {
@@ -73,7 +77,7 @@ var dashboard = {
         window.location.port = config.admin_port;
         return;
       }
-      window.location = window.location.protocol + '/' + '/' + window.location.hostname + ":8889/html/admin-dashboard.html";
+      window.location = window.location.protocol + '/' + '/' + window.location.hostname + ":"+config.admin_port+"/html/admin-dashboard.html";
     }, function(err) {
       SpinalUserManager.get_user_id("http://" + config.host + ":" + config.user_port + "/", user, password, function(response) {
         dashboard.save_user_local(user, password, config.user_port);
@@ -81,7 +85,7 @@ var dashboard = {
           window.location.port = config.user_port;
           return;
         }
-        window.location = window.location.protocol + '/' + '/' + window.location.hostname + ":8888/html/dashboard.html";
+        window.location = window.location.protocol + '/' + '/' + window.location.hostname + ":"+config.user_port+"/html/dashboard.html";
       }, function(err) {
         console.log("Error connect")
         dashboard.error_connect();
